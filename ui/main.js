2921bbce610fa542146c533b78db84851eb71a37
svkrdj <30564616+svkrdj@users.noreply.github.com>
2017-08-26 15:30:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 console.log('loaded');
+
+var img=document.getElementById('madi');
+var marginleft=0;
+function moveRight()
+{
+    marginleft=marginleft+5;
+    img.style.marginleft= marginleft + 'px';
+}
+img.onclick = function() {
+    var interval=setInterval(moveright,50);
+};
