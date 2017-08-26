5a1fb7252dcaf022d7b86fe2bf6be9a95a27e3fe
svkrdj <30564616+svkrdj@users.noreply.github.com>
2017-08-26 17:03:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,3 +21,29 @@ yo.onload= function(){
     
     
 };
+var submit= document.getElementById(submit_btn);
+submit.onclick= function() {
+       var request = new XMLHttpRequest();
+    
+    
+    request.onreadystatechange = function(){
+      if(request.readyState === XMLHttpRequest.DONE){
+          if(request.status === 200)
+          {
+          var names =request.responseText;
+          names= JSON.parse(names);
+          var list ='';
+          for (var i=0; i< names.length; i++)
+          {
+                 list += '<li>' +names [i] +'</li>';
+          }
+          val ul = document.getElementById('namelist');
+          ul.innerHTML= list;
+          }
+      }    
+    };
+    var ninput= document.getElementById('name');
+    var name =ninput.value;
+    request.open('GET', 'http://svkrclg.imad.hasura-app.io/submit-name?name='+name, true);
+    request.send(null);
+};
